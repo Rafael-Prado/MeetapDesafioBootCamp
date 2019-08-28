98d677416d29feac493ea9cf9a7a73f4a5e80cd0
Rafael da cruz do prado <rafaeldacruzdoprado@hotmail.com>
2019-08-27 21:17:18 -0300

    Token session added for user login

---
+++ src/app/controllers/SessionController.js
@@ -0,0 +1,31 @@
+import jwt from 'jsonwebtoken';
+import User from '../models/User';
+import authConf from '../../config/auth';
+
+class SesssionController {
+  async store(req, res) {
+    const { email, password } = req.body;
+
+    const user = await User.findOne({ where: { email } });
+    if (!user) {
+      return res.status(401).json({ error: 'User not found' });
+    }
+    if (!(await user.checkPassword(password))) {
+      return res.status(401).json({ error: 'Password does not march' });
+    }
+    const { id, name } = user;
+
+    return res.json({
+      user: {
+        id,
+        name,
+        email,
+      },
+      token: jwt.sign({ id }, authConf.secret, {
+        expiresIn: authConf.experesIn,
+      }),
+    });
+  }
+}
+
+export default new SesssionController();
